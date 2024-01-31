b164d2b679f1872f0b6ea558cc00591ecd724d5c
ileana1maria <156936418+ileana1maria@users.noreply.github.com>
2024-01-31 19:28:44 +0200

    Initial Project Structure/ Navbar- color

---
+++ app/src/main/java/com/example/androidapp/ui/journalingscreen/Note.kt
@@ -0,0 +1,11 @@
+//import android.room.Entity
+//import java.sql.Timestamp
+//
+//@Entity
+//data class Note(
+//    val title: String,
+//    val content: String,
+//    val timestamp: Long,
+//    val color: Int,
+//    @Primary
+//)
